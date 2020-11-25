55bfd360f01679efda1af42580c999fe74b20694
Flavio Souza <flavioluiz.ssouza@gmail.com>
2020-11-25 13:39:32 -0300

    best practice

--- epidemicSimulation.py
+++ epidemicSimulation.py
@@ -8,57 +8,69 @@ from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend
 from ndlib.models.ContinuousModel import ContinuousModel
 from ndlib.models.compartments.NodeStochastic import NodeStochastic
 
-def configureModel(g):
+S_CONS = "Susceptible"
+I_CONS = "Infected"
+R_CONS = "Removed"
+
+def configureModel(g, beta, gamma, fraction_infected):
     # Composite Model instantiation
     #model = DynamicCompositeModel(g)
     model = gc.CompositeModel(g)
     #model = ContinuousModel(g)
 
     # Model statuses
-    model.add_status("Susceptible")
-    model.add_status("Infected")
-    model.add_status("Removed")
+    model.add_status(S_CONS)
+    model.add_status(I_CONS)
+    model.add_status(R_CONS)
 
     # Compartment definition
-    c1 = NodeStochastic(0.02, triggering_status="Infected")
-    c2 = NodeStochastic(0.01)
+    c1 = NodeStochastic(beta, triggering_status=I_CONS)
+    c2 = NodeStochastic(gamma)
 
     # Rule definition
-    model.add_rule("Susceptible", "Infected", c1)
-    model.add_rule("Infected", "Removed", c2)
+    model.add_rule(S_CONS, I_CONS, c1)
+    model.add_rule(I_CONS, R_CONS, c2)
 
     # Model initial status configuration
     config = mc.Configuration()
-    config.add_model_parameter('fraction_infected', 0.3)
+    config.add_model_parameter('fraction_infected', fraction_infected)
     model.set_initial_status(config)
 
     return model
 
-def epidemicSimulation(model):
+def epidemicSimulation(model, iteration):
     # Simulation execution
-    iterations = model.iteration_bunch(200)
+    iterations = model.iteration_bunch(iteration)
     #model.visualize(iterations)
-    #iterations = model.iteration_bunch(100, node_status=False)
+    #iterations = model.iteration_bunch(iteration, node_status=False)
     trends = model.build_trends(iterations)
 
     return iterations, trends 
 
-def visualizacao(model, trends):
+def view(model, trends):
     #Visualizacao
     viz = DiffusionTrend(model, trends)
-    viz.plot("diffusion.pdf", percentile=90)
+    viz.plot("result/diffusion.pdf", percentile=90)
 
 def findCommunities(g):
     # Find the communities
     communities = sorted(nxcom.greedy_modularity_communities(g), key=len, reverse=True)
     # Count the communities
-    print("The karate club has", len(communities), "communities.")
+    print("The network has", len(communities), "communities.")
 
 if __name__ == "__main__":
+    #init variable
+    beta = 0.02
+    gamma = 0.01
+    fraction_infected = 0.1
+    iterations = 200 #iterations
+
     # Generate spatial network with communities
     g = generateSpatialGraph()
-    model = configureModel(g)
-    iterations, trends = epidemicSimulation(model)
-    visualizacao(model, trends)
+    
     #findCommunities(g)
+    model = configureModel(g, beta, gamma, fraction_infected)
+    iterations, trends = epidemicSimulation(model, iterations)
+    view(model, trends)
+    
     pass
