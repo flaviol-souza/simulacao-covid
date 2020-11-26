6715716657e68eeecdc276193d55f32a37863ef7
Flavio Souza <flavioluiz.ssouza@gmail.com>
2020-11-26 13:22:56 -0300

    last version to presentation

--- epidemicSimulation.py
+++ epidemicSimulation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import ndlib.models.ModelConfig as mc
 import ndlib.models.CompositeModel as gc
 import networkx.algorithms.community as nxcom
@@ -14,41 +15,79 @@ S_CONS = "Susceptible"
 I_CONS = "Infected"
 R_CONS = "Removed"
 
-initial_status = {
-    S_CONS: 0,
-    I_CONS: 1,
-    R_CONS: 2
-}
+def initial_status_I(node, graph, status, constants):
+    return np.random.uniform(0, constants['fraction_infected'])
 
-def configureModel(g, beta, gamma, fraction_infected):
+def update_S(node, graph, status, attributes, constants):
+    return status[node][I_CONS] * constants['beta']
+
+def update_I(node, graph, status, attributes, constants):
+    return status[node][R_CONS] * constants['gamma']
+
+def configureModel(g, beta, gamma, fraction_infected, withContinuousModel):
+    constants = {
+        'fraction_infected': fraction_infected,
+        'beta': beta,
+        'gamma': gamma
+    }
+
     # Composite Model instantiation
-    #model = DynamicCompositeModel(g)
-    model = gc.CompositeModel(g)
-    #model = ContinuousModel(g)
+    if withContinuousModel:
+        model = ContinuousModel(g, constants=constants)
+    else:
+        model = gc.CompositeModel(g)
 
     # Model statuses
     model.add_status(S_CONS)
     model.add_status(I_CONS)
     model.add_status(R_CONS)
 
-    # Compartment definition
-    c1 = NodeStochastic(beta, triggering_status=I_CONS)
-    c2 = NodeStochastic(gamma)
-
-    # Rule definition
-    model.add_rule(S_CONS, I_CONS, c1)
-    model.add_rule(I_CONS, R_CONS, c2)
-
     # Model initial status configuration
     config = mc.Configuration()
-    config.add_model_parameter('fraction_infected', fraction_infected)
-    model.set_initial_status(config)
+    
+    if type(model) is gc.CompositeModel:
+        # Compartment definition
+        c1 = NodeStochastic(beta, triggering_status=I_CONS)
+        c2 = NodeStochastic(gamma)
+
+        # Rule definition
+        model.add_rule(S_CONS, I_CONS, c1)
+        model.add_rule(I_CONS, R_CONS, c2)
+        config.add_model_parameter('fraction_infected', fraction_infected)
+        model.set_initial_status(config)    
+    elif type(model) is ContinuousModel: #just to ContinuousModel
+        condition = NodeStochastic(1)
+
+        # Rules
+        model.add_rule(S_CONS, update_S, condition, ['all'])
+        model.add_rule(I_CONS, update_I, condition, ['all'])
+        
+        initial_status = {
+            S_CONS: 1,
+            I_CONS: initial_status_I,
+            R_CONS: 1
+        }
+        model.set_initial_status(initial_status, config)
+        visualization_config = {
+            'layout': 'fr',
+            'plot_interval': 5,
+            'plot_variable': I_CONS,
+            'variable_limits': {
+                I_CONS: [0, 1]
+            },
+            'show_plot': False,
+            'cmin': -1,
+            'cmax': 1,
+            'color_scale': 'RdBu',
+            'plot_output': './result/animated.gif',
+            'plot_title': 'Animated network',
+        }
+        model.configure_visualization(visualization_config)
 
     return model, config
 
 def epidemicSimulation(model, iteration):
     iterations = model.iteration_bunch(iteration)
-    #iterations = model.iteration_bunch(iteration, node_status=False)
     trends = model.build_trends(iterations)
 
     return iterations, trends 
@@ -56,9 +95,12 @@ def epidemicSimulation(model, iteration):
 def multEpidemicSimulation(model, n_execution, n_iteration, infection_sets, n_processes):
     return multi_runs(model, execution_number=n_execution, iteration_number=n_iteration, infection_sets=infection_sets, nprocesses=n_processes)
 
-def view(model, trends):
-    viz = DiffusionTrend(model, trends)
-    viz.plot(PATH_RESULT+"diffusion.pdf", percentile=90)
+def view(model, trends, iterations=None):
+    if type(model) is gc.CompositeModel:
+        viz = DiffusionTrend(model, trends)
+        viz.plot(PATH_RESULT+"diffusion.pdf", percentile=90)
+    elif type(model) is ContinuousModel:
+        model.visualize(iterations)
 
 def findCommunities(g):
     # Find the communities
@@ -68,6 +110,7 @@ def findCommunities(g):
 
 if __name__ == "__main__":
     #boot variables
+    withContinuousModel = False
     mult_executions = False
     beta = 0.02
     gamma = 0.01
@@ -78,17 +121,17 @@ if __name__ == "__main__":
     g = generateSpatialGraph()
     
     #findCommunities(g)
-    model, config = configureModel(g, beta, gamma, fraction_infected)
+    model, config = configureModel(g, beta, gamma, fraction_infected, withContinuousModel)
     trends = None
     if mult_executions:
         n_execution = 10
         infection_sets = None
         n_processes = 1
         trends = multEpidemicSimulation(model, n_execution, n_iterations, infection_sets, n_processes)
+        view(model, trends)
     else:
         iterations, trends = epidemicSimulation(model, n_iterations)    
+        view(model, trends, iterations)
     
-    view(model, trends)
-
     print("Simulation completed.")
     pass
